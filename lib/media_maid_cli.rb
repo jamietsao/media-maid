731c0a96650b15d6688f2cc7705766c487e45bd0
Jamie Tsao <jamie@gametime.co>
2015-10-18 10:53:36 -0700

    Improved logging

--- lib/media_maid_cli.rb
+++ lib/media_maid_cli.rb
@@ -7,7 +7,6 @@ require 'yaml'
 # Media Maid CLI
 #
 class MediaMaidCLI < Thor
-  # TODO
   IGNORE_LIST = %w(. .. .DS_Store .picasa.ini)
   DIFF_THRESHOLD_IN_MILLIS = 1000;
 
@@ -30,7 +29,7 @@ class MediaMaidCLI < Thor
   desc 'all_exif', 'TODO'
   def all_exif(file)
     exif = MiniExiftool.new(file)
-    puts exif.to_yaml
+    log exif.to_yaml
   end
 
   desc 'fix_mtime', 'TODO'
@@ -44,17 +43,17 @@ class MediaMaidCLI < Thor
         mtime = File.mtime(source_dir + file)
         diff = (mtime - event_time).abs
         if diff > DIFF_THRESHOLD_IN_MILLIS
-          update_mtime(source_dir + file, event_time, options[:test])
+          update_mtime(source_dir + file, event_time)
           fixed += 1
         else
-          puts "event_time and mtime for #{file} are equal: #{event_time} - #{'NO FIX NEEDED'.magenta}" if options[:verbose]
+          log "event_time and mtime for #{file} are equal: #{event_time} - #{'NO FIX NEEDED'.blue}" if options[:verbose]
           not_needed += 1
         end
       else
         missing += 1
       end
     end
-    puts "SUMMARY --> Fixed: #{fixed.to_s.green} Not Needed: #{not_needed.to_s.green} Missing: #{missing.to_s.green}"
+    log "SUMMARY --> Fixed: #{fixed.to_s.green} Not Needed: #{not_needed.to_s.green} Missing: #{missing.to_s.green}"
   end
 
   private
@@ -64,30 +63,30 @@ class MediaMaidCLI < Thor
     if (filename.end_with?('JPG') or filename.end_with?('jpg'))
       exif = MiniExiftool.new(file_path)
       if options[:verbose]
-        puts "#{filename.green}"
-        puts "#{'SubSecDateTimeOriginal:'.ljust(30)} #{exif['SubSecDateTimeOriginal']}"
-        puts "#{'DateTimeOriginal:'.ljust(30)} #{exif['DateTimeOriginal']}"
-        puts "#{'FileModifyDate:'.ljust(30)} #{exif['FileModifyDate']}"
+        log "#{filename.green}"
+        log "#{'SubSecDateTimeOriginal:'.ljust(30)} #{exif['SubSecDateTimeOriginal']}"
+        log "#{'DateTimeOriginal:'.ljust(30)} #{exif['DateTimeOriginal']}"
+        log "#{'FileModifyDate:'.ljust(30)} #{exif['FileModifyDate']}"
       end
       exif['SubSecDateTimeOriginal'] ? exif['SubSecDateTimeOriginal'] : exif['DateTimeOriginal']
     elsif (filename.end_with?('MOV') or filename.end_with?('mov'))
       exif = MiniExiftool.new(file_path)
       if options[:verbose]
-        puts "#{filename.green}"
-        puts "#{'ContentCreateDate:'.ljust(30)} #{exif['ContentCreateDate']}"
-        puts "#{'CreateDate:'.ljust(30)} #{exif['CreateDate']}"
-        puts "#{'FileModifyDate:'.ljust(30)} #{exif['FileModifyDate']}"
+        log "#{filename.green}"
+        log "#{'ContentCreateDate:'.ljust(30)} #{exif['ContentCreateDate']}"
+        log "#{'CreateDate:'.ljust(30)} #{exif['CreateDate']}"
+        log "#{'FileModifyDate:'.ljust(30)} #{exif['FileModifyDate']}"
       end
       exif['ContentCreateDate'] ? exif['ContentCreateDate'] : exif['CreateDate']
     else
-      puts "#{filename.green} #{'UNRECOGNIZED FILE TYPE'.red}"
+      log "#{filename.green} #{'UNRECOGNIZED FILE TYPE'.red}"
       nil
     end
   end
 
-  def update_mtime(file, event_time, test)
-    #FileUtils.touch(file, :mtime => event_time)
-    puts "Updated mtime for #{file} to #{event_time} #{test ? 'TEST MODE'.magenta : ''}" if options[:verbose]
+  def update_mtime(file, event_time)
+    # FileUtils.touch(file, :mtime => event_time) unless options[:test]
+    log "Updated mtime for #{file} to #{event_time}" if options[:verbose]
   end
 
   def move_file(source_dir, dest_dir, filename, event_time)
@@ -105,5 +104,9 @@ class MediaMaidCLI < Thor
         move_file(source_dir_path, dest_dir_path, file, event_time)
       end
     end
+  end
+
+  def log(message)
+    puts "#{options[:test] ? '[TEST MODE] '.magenta : ''}#{message}"
   end
 end
