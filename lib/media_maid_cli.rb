addf87c45151774b23a094b926c570fe71b0c10b
Jamie Tsao <jamie@gametime.co>
2015-10-20 15:02:34 -0700

    Refactored fix_mtime. Added test for update_mtime(). Removed usage of SubSecDateTimeOriginal

--- lib/media_maid_cli.rb
+++ lib/media_maid_cli.rb
@@ -10,20 +10,6 @@ class MediaMaidCLI < Thor
   IGNORE_LIST = %w(. .. .DS_Store .picasa.ini)
   DIFF_THRESHOLD_IN_MILLIS = 1000
 
-  # input_source_dir_path = '/Users/jamie/Dropbox/Photos/Camera Sync/Test/'
-
-  # # copy source files to /tmp
-  # tmp_dest = '/tmp/media-maid/dest/'
-  # tmp_source = '/tmp/media-maid/source/'
-  # FileUtils.mkdir_p(tmp_source)
-  # FileUtils.cp_r(Dir.glob(input_source_dir_path + '*.*'), tmp_source, :preserve => true)
-
-  # source_dir_path = tmp_source
-  # dest_dir_path = tmp_dest
-
-  # # estimate(source_dir_path)
-  # clean(source_dir_path, dest_dir_path)
-
   class_option :verbose, type: :boolean
 
   desc 'fix_mtime', 'TODO'
@@ -32,18 +18,13 @@ class MediaMaidCLI < Thor
     missing, not_needed, fixed = 0, 0, 0
     Dir.foreach(source_dir) do |file|
       next if IGNORE_LIST.include? file
-      event_time = get_event_time(source_dir, file)
-      if event_time
-        mtime = File.mtime(source_dir + file)
-        diff = (mtime - event_time).abs
-        if diff > DIFF_THRESHOLD_IN_MILLIS
-          update_mtime(source_dir + file, event_time)
-          fixed += 1
-        else
-          log "event_time and mtime for #{file} are equal: #{event_time} - #{'NO FIX NEEDED'.blue}" if options[:verbose]
-          not_needed += 1
-        end
-      else
+      result = update_mtime(source_dir, file)
+      case result
+      when 1
+        fixed += 1
+      when 0
+        not_needed += 1
+      when -1
         missing += 1
       end
     end
@@ -52,18 +33,35 @@ class MediaMaidCLI < Thor
 
   private
 
+  def update_mtime(source_dir, file)
+    event_time = get_event_time(source_dir, file)
+    if event_time
+      mtime = File.mtime(source_dir + file)
+      diff = (mtime - event_time).abs
+      if diff > DIFF_THRESHOLD_IN_MILLIS
+        FileUtils.touch(source_dir + file, :mtime => event_time) unless options[:test]
+        log "Updated mtime for #{file} to #{event_time}" if options[:verbose]
+        return 1
+      else
+        log "event_time [#{event_time}] and mtime [#{mtime}] for are within threshold - #{'FIX NOT NEEDED'.blue}" if options[:verbose]
+        return 0
+      end
+    else
+      return -1
+    end
+  end
+
   def get_event_time(source_dir, filename)
-    file_path = source_dir + filename.downcase
-    if filename.end_with?('jpg')
+    file_path = source_dir + filename
+    if filename.downcase.end_with?('jpg')
       exif = MiniExiftool.new(file_path)
       if options[:verbose]
         log "#{filename.green}"
-        log "#{'SubSecDateTimeOriginal:'.ljust(30)} #{exif['SubSecDateTimeOriginal']}"
         log "#{'DateTimeOriginal:'.ljust(30)} #{exif['DateTimeOriginal']}"
         log "#{'FileModifyDate:'.ljust(30)} #{exif['FileModifyDate']}"
       end
-      exif['SubSecDateTimeOriginal'] ? exif['SubSecDateTimeOriginal'] : exif['DateTimeOriginal']
-    elsif filename.end_with?('mov')
+      exif['DateTimeOriginal']
+    elsif filename.downcase.end_with?('mov')
       exif = MiniExiftool.new(file_path)
       if options[:verbose]
         log "#{filename.green}"
@@ -78,26 +76,10 @@ class MediaMaidCLI < Thor
     end
   end
 
-  def update_mtime(file, event_time)
-    # FileUtils.touch(file, :mtime => event_time) unless options[:test]
-    log "Updated mtime for #{file} to #{event_time}" if options[:verbose]
-  end
-
   def move_file(source_dir, dest_dir, filename, event_time)
     sub_dir = "#{event_time.year}/#{event_time.strftime('%Y-%m')}-#{event_time.strftime('%B').downcase}"
     FileUtils.mkdir_p(dest_dir + sub_dir)
     FileUtils.mv(source_dir + filename, dest_dir + sub_dir)
-  end
-
-  def clean(source_dir_path, dest_dir_path)
-    Dir.foreach(source_dir_path) do |file|
-      next if file == '.' || file == '..'
-      event_time = get_event_time(source_dir_path, file)
-      if event_time
-        update_mtime(source_dir_path + file, event_time)
-        move_file(source_dir_path, dest_dir_path, file, event_time)
-      end
-    end
   end
 
   def log(message)
--- spec/media_maid_cli_spec.rb
+++ spec/media_maid_cli_spec.rb
@@ -5,29 +5,28 @@ describe MediaMaidCLI do
   describe '#get_event_time' do
     subject { described_class.new.send(:get_event_time, source_dir, filename) }
     let(:source_dir) { './spec/data/' }
-    let(:exif_subsecdatetimeoriginal) { MiniExiftool.new(source_dir + filename)['SubSecDateTimeOriginal'] }
     let(:exif_datetimeoriginal) { MiniExiftool.new(source_dir + filename)['DateTimeOriginal'] }
 
     context 'when \'jpg\' file' do
       context 'iPhone via Camera Sync' do
         let(:filename) { 'iPhone - Camera Sync.jpg' }
 
-        it 'should equal \'SubSecDateTimeOriginal\' EXIF date' do
-          is_expected.to eq exif_subsecdatetimeoriginal
+        it 'should equal \'DateTimeOriginal\' EXIF date' do
+          is_expected.to eq exif_datetimeoriginal
         end
       end
       context 'iPhone via USB' do
         let(:filename) { 'iPhone - USB.jpg' }
 
-        it 'should equal \'SubSecDateTimeOriginal\' EXIF date' do
-          is_expected.to eq exif_subsecdatetimeoriginal
+        it 'should equal \'DateTimeOriginal\' EXIF date' do
+          is_expected.to eq exif_datetimeoriginal
         end
       end
       context 'Canon T2i via USB' do
         let(:filename) { 'Canon T2i - USB.jpg' }
 
-        it 'should equal \'SubSecDateTimeOriginal\' EXIF date' do
-          is_expected.to eq exif_subsecdatetimeoriginal
+        it 'should equal \'DateTimeOriginal\' EXIF date' do
+          is_expected.to eq exif_datetimeoriginal
         end
       end
       context 'Canon ELPH via USB' do
@@ -39,4 +38,63 @@ describe MediaMaidCLI do
       end
     end
   end
+
+  describe '#update_mtime' do
+    before(:all) do
+      # create copy of test media files in tmp directory
+      @temp_dir = './spec/data/tmp/'
+      FileUtils.mkdir_p(@temp_dir)
+      FileUtils.cp_r(Dir.glob('./spec/data/' + '*.*'), @temp_dir, :preserve => true)
+    end
+    subject do
+      described_class.new.send(:update_mtime, temp_dir, filename)
+    end
+    let(:temp_dir) { @temp_dir }
+    let(:event_time) { described_class.new.send(:get_event_time, temp_dir, filename) }
+
+    context 'when \'jpg\' file' do
+      context 'iPhone via Camera Sync' do
+        let(:filename) { 'iPhone - Camera Sync.jpg' }
+
+        it 'updates file mtime to EXIF event time' do
+          expect(File.mtime(temp_dir + filename)).to_not eq event_time
+          is_expected.to eq 1
+          expect(File.mtime(temp_dir + filename)).to eq event_time
+        end
+      end
+      context 'iPhone via USB' do
+        let(:filename) { 'iPhone - USB.jpg' }
+
+        it 'does NOT update file mtime' do
+          expect(File.mtime(temp_dir + filename)).to eq event_time
+          is_expected.to eq 0
+          expect(File.mtime(temp_dir + filename)).to eq event_time
+        end
+      end
+      context 'Canon T2i via USB' do
+        let(:filename) { 'Canon T2i - USB.jpg' }
+
+        it 'does NOT update file mtime' do
+          expect(File.mtime(temp_dir + filename)).to eq event_time
+          is_expected.to eq 0
+          expect(File.mtime(temp_dir + filename)).to eq event_time
+        end
+      end
+      context 'Canon ELPH via USB' do
+        let(:filename) { 'Canon ELPH - USB.jpg' }
+
+        it 'does NOT update file mtime' do
+          # TODO: off by 1 second so this check fails =/
+          # expect(File.mtime(temp_dir + filename)).to eq event_time
+          is_expected.to eq 0
+          # expect(File.mtime(temp_dir + filename)).to eq event_time
+        end
+      end
+    end
+    after(:all) do
+      # remove temp directory after tests
+      FileUtils.rm_rf(@temp_dir)
+    end
+  end
+
 end
